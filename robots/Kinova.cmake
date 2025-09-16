e64bb3a0262b1c3e70d9d2e859c04c005f2b2e3a
mr-Sunio <sunio_112@hotmail.com>
2025-09-16 18:27:44 +0900

    fix merging

--- robots/Kinova.cmake
+++ robots/Kinova.cmake
@@ -1,25 +1,13 @@
 option(WITH_Kinova "Build Kinova support" OFF)
 
 if(NOT WITH_Kinova)
-<<<<<<< HEAD
-	return()
-=======
   return()
->>>>>>> mathieu/topic/AddKinova
 endif()
 
 if(NOT WITH_ROS_SUPPORT)
   message(FATAL_ERROR "ROS support is required to use the Kinova robot")
 endif()
 
-<<<<<<< HEAD
-AptInstall(
-  ros-${ROS_DISTRO}-gazebo-ros
-  ros-${ROS_DISTRO}-controller-manager
-  ros-${ROS_DISTRO}-control-msgs
-  ros-${ROS_DISTRO}-control-toolbox
-  ros-${ROS_DISTRO}-moveit-ros-planning-interface
-=======
 if(ROS_IS_ROS2)
   execute_process(
     COMMAND
@@ -42,7 +30,6 @@ endif()
 AptInstall(
   ros-${ROS_DISTRO}-controller-manager ros-${ROS_DISTRO}-control-msgs
   ros-${ROS_DISTRO}-control-toolbox ros-${ROS_DISTRO}-moveit-ros-planning-interface
->>>>>>> mathieu/topic/AddKinova
 )
 
 # Install conan if needed
@@ -52,37 +39,16 @@ if(NOT CONAN)
   if(NOT PYTHON3)
     message(FATAL_ERROR "Must have python3 to install conan to use the Kinova robot")
   endif()
-<<<<<<< HEAD
-  execute_process(COMMAND sudo python3 -m pip install conan==1.60.1 COMMAND_ERROR_IS_FATAL ANY)
-=======
   execute_process(
     COMMAND /usr/bin/python3 -m pip install --break-system-packages conan>=1.60.1
             COMMAND_ERROR_IS_FATAL ANY
   )
->>>>>>> mathieu/topic/AddKinova
   find_program(CONAN conan)
   if(NOT CONAN)
     message(FATAL_ERROR "Conan installation went wrong")
   endif()
   file(REAL_PATH ${CMAKE_C_COMPILER} CONAN_C_COMPILER)
   file(REAL_PATH ${CMAKE_CXX_COMPILER} CONAN_CXX_COMPILER)
-<<<<<<< HEAD
-  execute_process(COMMAND ${CMAKE_COMMAND} -E env CC=${CONAN_C_COMPILER} CXX=${CONAN_CXX_COMPILER} ${CONAN} profile new default --detect COMMAND_ERROR_IS_FATAL ANY)
-  execute_process(COMMAND ${CONAN} profile update settings.compiler.libcxx=libstdc++11 default COMMAND_ERROR_IS_FATAL ANY)
-endif()
-execute_process(COMMAND ${CONAN} config set general.revisions_enabled=1 COMMAND_ERROR_IS_FATAL ANY)
-
-AddCatkinProject(ros_kortex
-  GITHUB Kinovarobotics/ros_kortex
-  GIT_TAG origin/${ROS_DISTRO}-devel
-  WORKSPACE data_ws
-)
-
-AddProject(mc_kinova
-  GITHUB mathieu-celerier/mc_kinova
-	GIT_TAG origin/main
-	DEPENDS mc_rtc ros_kortex
-=======
   execute_process(
     COMMAND ${CMAKE_COMMAND} -E env CC=${CONAN_C_COMPILER} CXX=${CONAN_CXX_COMPILER}
             ${CONAN} profile new default --detect COMMAND_ERROR_IS_FATAL ANY
@@ -187,5 +153,4 @@ AddProject(
   GITHUB mathieu-celerier/mc_kinova
   GIT_TAG origin/topic/bota_ft_sensor_w_DS4
   DEPENDS mc_rtc ${ROS_KORTEX_NAME} bota_driver
->>>>>>> mathieu/topic/AddKinova
 )
